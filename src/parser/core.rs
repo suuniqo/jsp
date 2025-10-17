4c187bf7a2b2e6f66da629e9a6a5affd3b1340ca
suuniqo <suuniqo@gmail.com>
2025-10-17 10:14:41 +0200

    made parser pub

--- src/parser/core.rs
+++ src/parser/core.rs
@@ -1,6 +1,6 @@
 use crate::{context::{Context, symtable::SymTable}, lexer::Lexer};
 
-struct ParserCore<'t, 'c, T: SymTable, L: Lexer> {
+pub struct ParserCore<'t, 'c, T: SymTable, L: Lexer> {
     lexer: L,
     ctx: &'c mut Context<'t, T>
 }
--- test.js
+++ test.js
@@ -1,10 +1,30 @@
 if (cond) {
+    /* this string has an invalid escape sequence */
     read "hola\q"
 }
 
+
+/* this characters are illegal */
+$$ & | ?    @@?
+
+/* this string is unterminated */
 let dop = "lasdljkljdfsalkj
 
-let a = 029348028348230948092840923094809238409238409238
+/* this literal is out of range for i16 */
+let a = 029348028348230948092840923094809238409238409238;
+
+if (dop == a * cond) {
+    do {
+        write "death";
+    } while (cond);
+}
+
+dop &= a;
+
+let invf = 42342492834723948723432739487.87238972349837249872984
+
+/* this float has invalid format */
 let b = 38284239823948.
 
+/* this comment is unterminated */
 /* iasdlkhj
